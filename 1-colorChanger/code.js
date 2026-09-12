1c7045f35a2998d280bccc5df53b0b775e755c54
Sudhanshu4624 <98212727+Sudhanshu4624@users.noreply.github.com>
2026-09-12 12:36:46 +0000

    1st project - bg color changer

---
+++ 1-colorChanger/code.js
@@ -0,0 +1,66 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach(function (button) {
+    console.log(button);
+    button.addEventListener('click', function (e) {
+        console.log(e);
+        console.log(e.target);
+        switch (e.target.id) {
+            // case 'grey':
+            //     body.style.backgroundColor = e.target.id 
+            //     break;
+            // case 'white':
+            //     body.style.backgroundColor = e.target.id 
+            //     break;
+            // case 'blue':
+            //     body.style.backgroundColor = e.target.id 
+            //     break;
+            // case 'yellow':
+            //     body.style.backgroundColor = e.target.id 
+            //     break;
+            
+            // this can also be written as 
+            case 'grey':
+            case 'white':
+            case 'blue':
+            case 'yellow':
+                body.style.backgroundColor = e.target.id 
+                break;
+            default:
+                body.style.backgroundColor = 'orange'
+                break;
+        }
+    })
+});
+
+
+// Another way
+// const buttons = document.querySelectorAll('.button');
+// const body = document.querySelector('body');
+
+// buttons.forEach(function (button) {
+//   console.log(button);
+//   button.addEventListener('click', function (e) {
+//     console.log(e);
+//     console.log(e.target);
+//     if (e.target.id === 'grey') {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'white') {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'blue') {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === 'yellow') {
+//       body.style.backgroundColor = e.target.id;
+//     }
+    
+//   });
+// });
+
+// Advance way
+// const body = document.querySelector('body')
+// document.querySelectorAll('.button').forEach((button) => 
+// (button.addEventListener('click', (e) => (body.style.backgroundColor = e.target.id))))
